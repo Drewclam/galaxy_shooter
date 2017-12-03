f0296b6bd1bf192719d636a0a603350a01378737
Drewclam <andrew.lam@rangle.io>
2017-12-02 23:51:21 -0500

    Add play area boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	public float speed = 5.0f;
+	public float yMax = 4.231822f;
+
+	public float xMax = 8.310589f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,8 +18,20 @@ public class Player : MonoBehaviour {
 	void Update () {
 		float horizontalInput = Input.GetAxis("Horizontal");
 		float verticalInput = Input.GetAxis("Vertical");
-		
+
+		// Input and Movement
 		transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
 		transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
+
+		// Play Area Boundaries
+		if (transform.position.y > yMax) {
+			transform.position = new Vector3(transform.position.x, yMax, transform.position.z);
+		} else if (transform.position.y < -yMax) {
+			transform.position = new Vector3(transform.position.x, -yMax, transform.position.z);
+		} else if (transform.position.x > xMax) {
+			transform.position = new Vector3(-xMax, transform.position.y, transform.position.z);
+		} else if (transform.position.x < -xMax) {
+			transform.position = new Vector3(xMax, transform.position.y, transform.position.z);
+		}
 	}
 }
