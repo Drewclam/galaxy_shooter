c26d6cfa9a6bb47a7e5d9558dbcba67ef46d075a
Drewclam <andrew.lam@rangle.io>
2017-12-03 13:10:47 -0500

    Refactor movement logic into separate method

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-
+	
+	[SerializeField]
 	private float speed = 5.0f;
 	private float yMax = 4.231822f;
-
 	private float xMax = 9.481674f;
 
 	// Use this for initialization
-	void Start () {
+	private void Start () {
 		transform.position = new Vector3(0, 0, 0);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	private void Update () {
+		Movement();
+	}
+
+	private void Movement() {
 		float horizontalInput = Input.GetAxis("Horizontal");
 		float verticalInput = Input.GetAxis("Vertical");
 
