af5d0e2b1119bd6abb8ac84ee65c857b30e64bd4
Andrew Lam <andrew.c.lam94@gmail.com>
2017-12-13 21:59:07 -0500

    Add collider to tripleshot power up

---
+++ Assets/Scripts/TripleShotPowerUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TripleShotPowerUp : MonoBehaviour {
+
+	// [SerializeField] private GameObject _tripleShotPowerUpPreFab;
+	[SerializeField] private float _speed = 3.0F;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.down * _speed * Time.deltaTime);
+	}
+}
