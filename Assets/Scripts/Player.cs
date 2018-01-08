9a9bc2094a2a480ab9f3230eac9cc9f1aa3dd7f4
Andrew Lam <andrew.c.lam94@gmail.com>
2018-01-07 22:35:22 -0500

    Add speed power up and cd

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 	
 	public bool canTripleShot = false;
+	[SerializeField] private bool isSpeedUp = false;
 
 	[SerializeField] private GameObject _laserPrefab;
 	[SerializeField] private GameObject _tripleShotPrefab;
@@ -12,7 +13,8 @@ public class Player : MonoBehaviour {
 	private float _yMax = 4.231822f;
 	private float _xMax = 9.481674f;
 
-	[SerializeField] private float _speed = 5.0f;
+	[SerializeField] private float _speed = 5.0F;
+	private float _powerUpSpeed = 10.0F;
 	[SerializeField] private float _fireRate = 0.25F;
 	private float _coolDown = 0.0F;
 	
@@ -34,19 +36,14 @@ public class Player : MonoBehaviour {
 		float horizontalInput = Input.GetAxis("Horizontal");
 		float verticalInput = Input.GetAxis("Vertical");
 
-		// Input and Movement
-		transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-		transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+		setPlayBoundary();
 
-		// Play Area Boundaries
-		if (transform.position.y > _yMax) {
-			transform.position = new Vector3(transform.position.x, _yMax, transform.position.z);
-		} else if (transform.position.y < -_yMax) {
-			transform.position = new Vector3(transform.position.x, -_yMax, transform.position.z);
-		} else if (transform.position.x > _xMax) {
-			transform.position = new Vector3(-_xMax, transform.position.y, transform.position.z);
-		} else if (transform.position.x < -_xMax) {
-			transform.position = new Vector3(_xMax, transform.position.y, transform.position.z);
+		if (isSpeedUp) {
+			move(Vector3.right * horizontalInput, _powerUpSpeed);
+			move(Vector3.up * verticalInput, _powerUpSpeed);
+		} else {
+			move(Vector3.right * horizontalInput, _speed);
+			move(Vector3.up * verticalInput, _speed);
 		}
 	}
 
@@ -56,6 +53,42 @@ public class Player : MonoBehaviour {
 			Instantiate(_tripleShotPrefab, transform.position, Quaternion.identity);
 		} else {
 			Instantiate(_laserPrefab, new Vector3(transform.position.x, transform.position.y + 1.03f, transform.position.z), Quaternion.identity);
+		}
+	}
+
+	public void tripleShotPowerUp() {
+		canTripleShot = true;
+		StartCoroutine(tripleShotPowerDownRoutine());
+	}
+
+	private IEnumerator tripleShotPowerDownRoutine() {
+		yield return new WaitForSeconds(5.0F);
+		canTripleShot = false;
+	}
+
+	public void speedPowerUp() {
+		isSpeedUp = true;
+		StartCoroutine(speedPowerDownRoutine());
+	}
+
+	private IEnumerator speedPowerDownRoutine() {
+		yield return new WaitForSeconds(5.0F);
+		isSpeedUp = false;
+	}
+
+	private void move(Vector3 direction, float speed) {
+		transform.Translate(direction * speed * Time.deltaTime);
+	}
+
+	private void setPlayBoundary() {
+		if (transform.position.y > _yMax) {
+			transform.position = new Vector3(transform.position.x, _yMax, transform.position.z);
+		} else if (transform.position.y < -_yMax) {
+			transform.position = new Vector3(transform.position.x, -_yMax, transform.position.z);
+		} else if (transform.position.x > _xMax) {
+			transform.position = new Vector3(-_xMax, transform.position.y, transform.position.z);
+		} else if (transform.position.x < -_xMax) {
+			transform.position = new Vector3(_xMax, transform.position.y, transform.position.z);
 		}
 	}
 }
---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour {
+
+	// [SerializeField] private GameObject _tripleShotPowerUpPreFab;
+	[SerializeField] private float _speed = 3.0F;
+	[SerializeField] private int id;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.down * _speed * Time.deltaTime);
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		Player player = other.GetComponent<Player>();
+
+		if (other.tag == "Player" && player != null) {
+			if (id == 0) {
+				player.tripleShotPowerUp();
+			} else if (id == 1) {
+				// enable speed power up
+				player.speedPowerUp();
+			} else if (id == 2) {
+				// enable shield power up
+			}
+			Destroy(gameObject);
+		}
+	}
+}
