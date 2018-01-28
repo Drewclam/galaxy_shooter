53af855aed15b4935fc3d957019f7e96d3aaff17
Andrew Lam <andrew.c.lam94@gmail.com>
2018-01-28 17:20:01 -0500

    Fix syntax error

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,18 +6,19 @@ public class SpawnManager : MonoBehaviour {
 	[SerializeField] private GameObject enemyPrefab;
 	[SerializeField] private GameObject[] powerUps;
 
-	// Use this for initialization
-	IEnumerator Start () {
+	void Start () {
 		while (true) {
-			yield return new WaitForSeconds(5.0F);
-			StartCoroutine("spawnEnemy");
+			StartCoroutine(spawnEnemyRoutine());
 		}
 	}
 
-	private void spawnEnemy() {
+	private IEnumerator spawnEnemyRoutine() {
 		float randomX = Random.Range(-7.87F, 7.87F);
 		float yMax = 6.46F;
 
-		Instantiate(enemyPrefab, new Vector3(randomX, yMax, transform.position.z), Quaternion.identity);
+		while (true) {
+			Instantiate(enemyPrefab, new Vector3(randomX, yMax, transform.position.z), Quaternion.identity);
+			yield return new WaitForSeconds(5.0F);
+		}
 	}
 }
