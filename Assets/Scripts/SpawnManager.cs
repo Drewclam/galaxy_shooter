b24b9c005e20fc5f4f04b733315736a8b401faa2
Andrew Lam <andrew.c.lam94@gmail.com>
2018-01-28 13:08:53 -0500

    Add prefabs to spawn manager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+	[SerializeField] private GameObject enemyPrefab;
+	[SerializeField] private GameObject[] powerUps;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
