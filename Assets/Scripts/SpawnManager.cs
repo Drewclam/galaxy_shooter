234f71a88aecb5bc6ae57f9f5ecc3a98433f6f11
Andrew Lam <andrew.c.lam94@gmail.com>
2018-01-30 00:39:29 -0500

    Add player lives image

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,20 +8,20 @@ public class SpawnManager : MonoBehaviour {
 
 	void Start() {
 		StartCoroutine(spawnEnemyRoutine());
-		StartCoroutine(powerUpSpawnRoutine());
+		StartCoroutine(spawnPowerUpRoutine());
 	}
 
 	private IEnumerator spawnEnemyRoutine() {
-		float randomX = Random.Range(-7.87F, 7.87F);
 		float yMax = 6.46F;
 
 		while (true) {
+			float randomX = Random.Range(-7.87F, 7.87F);
 			Instantiate(enemyPrefab, new Vector3(randomX, yMax, transform.position.z), Quaternion.identity);
-			yield return new WaitForSeconds(5.0F);
+			yield return new WaitForSeconds(0.5F);
 		}
 	}
 
-	private IEnumerator powerUpSpawnRoutine() {
+	private IEnumerator spawnPowerUpRoutine() {
 		float yMax = 6.46F;
 
 		while (true) {
