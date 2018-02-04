1d679c849062f65068268569705c8f12b9d6ad16
Andrew Lam <andrew.c.lam94@gmail.com>
2018-02-03 19:16:53 -0500

    Add player animator controller and left/right turn animations

---
+++ Assets/Scripts/PlayerAnimationHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationHandler : MonoBehaviour {
+	private Animator animator;
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		// if w key pressed or left key pressed
+		if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+			animator.SetBool("isTurningLeft", true);
+			animator.SetBool("isTurningRight", false);
+		} else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow)) {
+			animator.SetBool("isTurningLeft", false);
+		} else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+			animator.SetBool("isTurningRight", true);
+		} else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow)) {
+			animator.SetBool("isTurningRight", false);
+		}
+			// set the isTUrningLeft to true
+		// else if w key up or left key up
+			// set isTurningLeft to false
+	}
+}
